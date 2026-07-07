042e03d0e65bc0b05af7003d775cd86be40b64df
Amelia Goodson <ameliajanegoodson@gmail.com>
2026-07-07 18:21:57 +1000

    Add: is-subsequence - submission-3

---
+++ Data Structures & Algorithms/is-subsequence/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i +=1
+            j += 1
+        print(i)
+        print(j)
+        
+        if i == len(s):
+            return True
+        else:
+            return False
